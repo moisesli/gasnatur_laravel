807c4f7c8ebab0b1c91c8fe8ae522d59d7dd2320
moises <elnaufrago2009@gmail.com>
2021-11-25 11:27:08 -0500

    add migration, model and controller to Empresa. fix 3

---
+++ app/Models/Empresa.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empresa extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'ruc',
+        'razon_social',
+        'nombre_comercial',
+        'direccion',
+        'telefono',
+        'celular',
+        'correo',
+        'web',
+        'logo',
+        'estado'
+    ];
+}
---
+++ database/migrations/2021_11_25_152221_create_empresas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->string('ruc', 11);
+            $table->string('razon_social', 150);
+            $table->string('nombre_comercial', 200);
+            $table->string('direccion', 150);
+            $table->string('telefono', 100);
+            $table->string('celular', 100);
+            $table->string('correo', 100);
+            $table->string('web', 150);
+            $table->string('logo', 150);
+            $table->string('estado', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
