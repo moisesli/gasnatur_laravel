b409091ca3e1497b5a9a78d1518e7519a7adc6a7
moises <elnaufrago2009@gmail.com>
2021-11-26 12:18:58 -0500

    add token template to project. fixe #4

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -3,8 +3,12 @@
 <head>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Laravel') }}</title>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   <!-- Tailwind -->
   <link rel="stylesheet" href="{{ asset('theme/css/tailwind.output.css') }}"/>
 
